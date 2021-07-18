a275f95692dd94cad6c72ac7fe677754d2340d13
Me <me@Morgans-MacBook-Pro.local>
2021-07-18 18:46:35 +0700

    Try to setup LIFF with React and Typescript

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import liff from '@line/liff'; 
+import './App.css';
+
+function App() {
+    const sendMessage = () => {
+        liff.init({liffId: `${process.env.MY_LIFF_ID}`}).then(() => {
+            liff.sendMessages([
+                {
+                    type: 'text',
+                    text: 'Hello, React World!'
+                }
+            ])
+            .then(() => {
+                console.log('message sent');
+            })
+            .catch((err) => {
+                console.log('error', err);
+            });
+        })
+    }
+    
+    return (
+      <div className="App">
+        <button className="button" onClick={sendMessage}>
+           Send Message
+        </button>
+      </div>
+    )
+}
+
+export default App;
